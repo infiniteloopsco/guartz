ad95070b5b16bdeeff6dd48138e97473183c4f92
Michel Perez <michel.ingesoft@gmail.com>
2015-08-27 10:49:34 -0500

    added some logs

--- models/jobs.go
+++ models/jobs.go
@@ -14,15 +14,15 @@ var MasterCron *cron.Cron
 func InitCron() {
 	MasterCron = cron.New()
 	MasterCron.Start()
-	// Gdb.Model(Task{}).Where("cron_id != 0").Updates(Task{CronID: 0})
 
 	//Loads the tasks on the cron system
 	var tasks []Task
 	Gdb.Find(&tasks)
-	fmt.Printf("***STARTING %d PROJECTS***\f", len(tasks))
+	fmt.Printf("***STARTING %d PROJECTS***\n", len(tasks))
 	InTx(func(txn *gorm.DB) bool {
 		for _, task := range tasks {
 			if err := task.Start(txn); err != nil {
+				fmt.Print(err)
 				return false
 			}
 		}
