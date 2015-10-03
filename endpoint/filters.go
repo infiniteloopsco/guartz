37afa38529577ab9566eddf80db1aaffe84e3689
Michel Perez <michel.ingesoft@gmail.com>
2015-10-03 09:32:21 -0500

    removed filter

--- endpoint/filters.go
+++
@@ -1,9 +0,0 @@
-package endpoint
-
-import "github.com/gin-gonic/gin"
-
-func AngularFilter(c *gin.Context) {
-	c.Header("Access-Control-Allow-Origin", "*")
-	c.Header("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD")
-	c.Header("Access-Control-Allow-Headers", "*,x-requested-with,__setXHR_,Content-Type,If-Modified-Since,If-None-Match")
-}
